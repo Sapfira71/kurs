0b955420c4a65a9e324b46568deb33367d6de333
Morozova Anastasia <a.morozova@maximaster.ru>
2016-02-09 12:15:26 +0300

    Made the task of working with data

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 435
- * Date: 09.02.2016
- * Time: 10:24
- */
-echo 'Привет';
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+    <?php
+    echo "<table border='1'>";
+    if (($handle = fopen("products.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            foreach ($data as $value) {
+                $elements = explode(";", $value);
+                $num = count($elements);
+
+                echo "<tr>";
+                for ($i = 0; $i < $num; $i++) {
+                    echo "<td>" . $elements[$i] . "</td>";
+                }
+                echo "</tr>";
+            }
+        }
+        fclose($handle);
+    }
+    echo "</table>";
+    ?>
+</body>
+</html>
