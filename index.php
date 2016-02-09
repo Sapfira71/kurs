fc8abdeea5155677293dabb387f26f19a78f5232
Morozova Anastasia <a.morozova@maximaster.ru>
2016-02-09 16:39:45 +0300

    Added view of data from infoblock

--- index.php
+++ index.php
@@ -4,55 +4,56 @@
     <link rel="stylesheet" href="styles.css"/>
 </head>
 <body>
+        <?php
+        include $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php';
+        CModule::IncludeModule('iblock');
 
-<?
-include $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php';
-CModule::IncludeModule('iblock');
-if (($handle = fopen("products.csv", "r")) !== FALSE) {
-    $counter = 0;
-    $datarr = array();
-    $keys = array("ART", "NAME", "QUANTITY", "PRICE", "SYMB");
-    while (($data = fgetcsv($handle)) !== FALSE) {
-        foreach ($data as $value) {
-            $elements = explode(";", $value);
-
-            $el = array();
-            foreach ($elements as $it){
-                $el[$keys[$counter]] = $it;
-                $counter++;
-            }
+        if (($handle = fopen("products.csv", "r")) !== FALSE) {
             $counter = 0;
-            $datarr[] = $el;
-        }
-    }
+            $datarr = array();
+            $keys = array("ART", "NAME", "QUANTITY", "PRICE", "SYMB");
+            while (($data = fgetcsv($handle)) !== FALSE) {
+                foreach ($data as $value) {
+                    $elements = explode(";", $value);
 
-    foreach ($datarr as $elem) {
-        $ibe = new CIBlockElement;
-        $PROP = array();
-        $PROP['ART'] = $elem['ART'];
-        $PROP['NAME'] = $elem['NAME'];
-        $PROP['QUANTITY'] = $elem['QUANTITY'];
-        $PROP['PRICE'] = $elem['PRICE'];
-        $PROP['SYMB'] = $elem['SYMB'];
+                    $el = array();
+                    foreach ($elements as $it){
+                        $el[$keys[$counter]] = $it;
+                        $counter++;
+                    }
+                    $counter = 0;
+                    $datarr[] = $el;
+                }
+            }
 
-        print_r($PROP);
-        echo "<br/>";
-        echo "<br/>";
-        $arFields = Array(
-            "ACTIVE" => 'Y',
-            "IBLOCK_ID" => 1,
-            "PROPERTY_VALUES"=> $PROP,
-        );
-        if($ID = $ibe->Add($arFields)){
-            echo 'New ID: '.$ID.'<br>';
-        }
-        else{
-            echo 'Error: '.$ibe->LAST_ERROR.'<br>';
-        }
-    }
-    fclose($handle);
-}
-?>
+            foreach ($datarr as $elem) {
+                $ibe = new CIBlockElement;
+                $PROP = array();
+                $PROP['ART'] = $elem['ART'];
+                $PROP['NAME'] = $elem['NAME'];
+                $PROP['QUANTITY'] = $elem['QUANTITY'];
+                $PROP['PRICE'] = $elem['PRICE'];
+                $PROP['SYMB'] = $elem['SYMB'];
 
+                $arFields = Array(
+                    "ACTIVE" => 'Y',
+                    "IBLOCK_ID" => 1,
+                    'NAME' => 'product',
+                    "PROPERTY_VALUES"=> $PROP,
+                );
+                if($ID = $ibe->Add($arFields)){
+                    $res = CIBlockElement::GetList(Array(), Array(), false, Array("nPageSize"=>5), Array());
+                    while($arProduct = $res->GetNext())
+                    {
+                        print_r($arProduct);
+                    }
+                }
+                else{
+                    echo 'Error: '.$ibe->LAST_ERROR.'<br>';
+                }
+            }
+            fclose($handle);
+        }
+        ?>
 </body>
 </html>
