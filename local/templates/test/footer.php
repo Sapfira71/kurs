c60f3c5302b103ce0371b2445d8eb20d31672722
Morozova Anastasia <a.morozova@maximaster.ru>
2016-02-20 14:58:15 +0300

    Подключение стилей и скриптов спец.функциями. В файле стилей вместо переобпределения вида некоторых тегов созданы классы. Скрипт формирования ссылки вынесен в отдельный файл

--- local/templates/test/footer.php
+++ local/templates/test/footer.php
@@ -7,7 +7,7 @@
         </td>
     </tr>
 </table>
-<footer>
+<footer class="footer">
     <a href="mailto:a.morozova@maximaster.ru?subject=<?=selfURL();?>&body=Hello! It`s your site">
         Морозова Анастасия Олеговна
     </a>
--- local/templates/test/header.php
+++ local/templates/test/header.php
@@ -1,33 +1,24 @@
 <!DOCTYPE>
 <html>
 <head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+    <? $APPLICATION->SetAdditionalCSS("<?= SITE_TEMPLATE_PATH ?>/styles.css"); ?>
+    <script type="text/javascript"
+            src="<?= CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH . '/scripts.js', true) ?>"></script>
     <? $APPLICATION->ShowHead() ?>
     <title><? $APPLICATION->ShowTitle() ?></title>
-    <link rel="stylesheet" href="<?= SITE_TEMPLATE_PATH ?>/styles.css"/>
-
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-    <script src="<?= SITE_TEMPLATE_PATH ?>/scripts.js"></script>
     <!--[if lt IE 9]>
+
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 </head>
 <body>
-<?php
-$APPLICATION->ShowPanel();
-function selfURL()
-{
-    $url = ($_SERVER["HTTPS"] != 'on') ? 'http://' . $_SERVER["SERVER_NAME"] : 'https://'
-        . $_SERVER["SERVER_NAME"];
-    $url .= ($_SERVER["SERVER_PORT"] != 80) ? ":" . $_SERVER["SERVER_PORT"] : "";
-    $url .= $_SERVER["REQUEST_URI"];
-    return $url;
-}
-
-?>
-<header><img src="<?= SITE_TEMPLATE_PATH ?>/images/lotus.png" class="imgLotus">
+<?php require('scriptsphp.php'); ?>
+<header class="header"><img src="<?= SITE_TEMPLATE_PATH ?>/images/lotus.png" class="imgLotus">
     Привет! Этот сайт - результат выполнения обучающего задания.
 </header>
-<menu type="toolbar" id="headMenu">
+<menu class="menu" type="toolbar">
     <a href="/">Главная страница</a>
     <a href="../about.php">О себе</a>
     <a href="../contacts.php">Контакты</a>
