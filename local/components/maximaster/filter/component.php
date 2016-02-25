dded96e194784d23417cea03a47c20a7767d2061
Морозова <a.morozova@maximaster.ru>
2016-02-25 16:14:06 +0300

    В фильтре добавлено заполнение полей и массива для фильтрации, используя параметры полученные из GET-запроса

--- local/components/maximaster/filter/component.php
+++ local/components/maximaster/filter/component.php
@@ -2,29 +2,54 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
+
 $arFilter = Array();
-if (isset($arParams["FILTER"]["availability"])) {
+
+if (isset($arParams["FILTER"]["availability"]) || !empty($_GET["availability"])) {
     $arFilter['!PROPERTY_QUANTITY'] = 0;
+    $arResult["availability"] = 'on';
 }
+
 if (!empty($arParams["FILTER"]["name"])) {
-    $arFilter['NAME'] = $arParams["FILTER"]["name"];
+    $arFilter['NAME'] = $arResult["name"] = $arParams["FILTER"]["name"];
+} elseif (!empty($_GET["name"])) {
+    $arFilter['NAME'] = $arResult["name"] = $_GET["name"];
 }
+
 if (!empty($arParams["FILTER"]["vendorCode"])) {
-    $arFilter['PROPERTY_ART'] = $arParams["FILTER"]["vendorCode"];
+    $arFilter['PROPERTY_ART'] = $arResult["vendorCode"] = $arParams["FILTER"]["vendorCode"];
+} elseif (!empty($_GET["vendorCode"])) {
+    $arFilter['PROPERTY_ART'] = $arResult["vendorCode"] = $_GET["vendorCode"];
 }
+
 if (!empty($arParams["FILTER"]["price1"]) && !empty($arParams["FILTER"]["price2"])) {
     $arFilter['><PROPERTY_PRICE'] = Array(
         $arParams["FILTER"]["price1"],
         $arParams["FILTER"]["price2"]
     );
+    $arResult["price1"] = $arParams["FILTER"]["price1"];
+    $arResult["price2"] = $arParams["FILTER"]["price2"];
+} elseif (!empty($_GET["price1"]) && !empty($_GET["price2"])) {
+    $arFilter['><PROPERTY_PRICE'] = Array(
+        $_GET["price1"],
+        $_GET["price2"]
+    );
+    $arResult["price1"] = $_GET["price1"];
+    $arResult["price2"] = $_GET["price2"];
 } else {
     if (!empty($arParams["FILTER"]["price1"])) {
-        $arFilter['>PROPERTY_PRICE'] = $arParams["FILTER"]["price1"];
+        $arFilter['>PROPERTY_PRICE'] = $arResult["price1"] = $arParams["FILTER"]["price1"];
+    } elseif (!empty($_GET["price1"])) {
+        $arFilter['>PROPERTY_PRICE'] = $arResult["price1"] = $_GET["price1"];
     }
+
     if (!empty($arParams["FILTER"]["price2"])) {
-        $arFilter['<PROPERTY_PRICE'] = $arParams["FILTER"]["price2"];
+        $arFilter['<PROPERTY_PRICE'] = $arResult["price2"] = $arParams["FILTER"]["price2"];
+    } elseif (!empty($_GET["price2"])) {
+        $arFilter['<PROPERTY_PRICE'] = $arResult["price2"] = $_GET["price2"];
     }
 }
 
 $_POST['myFilter'] = $arFilter;
+
 $this->IncludeComponentTemplate();
--- local/components/maximaster/filter/templates/.default/template.php
+++ local/components/maximaster/filter/templates/.default/template.php
@@ -3,35 +3,35 @@
         <tr>
             <td>Артикул:</td>
             <td><input type="text" name="myFilter[vendorCode]"
-                       value="<? if (isset($arParams["FILTER"]["vendorCode"])) {
-                           echo $arParams["FILTER"]["vendorCode"];
+                       value="<? if (isset($arResult["vendorCode"])) {
+                           echo $arResult["vendorCode"];
                        } ?>">
             </td>
         </tr>
         <tr>
             <td>Название:</td>
             <td><input type="text" name="myFilter[name]"
-                       value="<? if (isset($arParams["FILTER"]["name"])) {
-                           echo $arParams["FILTER"]["name"];
+                       value="<? if (isset($arResult["name"])) {
+                           echo $arResult["name"];
                        } ?>">
             </td>
         </tr>
         <tr>
             <td>Цена:</td>
             <td>от <input type="text" name="myFilter[price1]"
-                          value="<? if (isset($arParams["FILTER"]["price1"])) {
-                              echo $arParams["FILTER"]["price1"];
+                          value="<? if (isset($arResult["price1"])) {
+                              echo $arResult["price1"];
                           } ?>">
                 до <input type="text" name="myFilter[price2]"
-                          value="<? if (isset($arParams["FILTER"]["price2"])) {
-                              echo $arParams["FILTER"]["price2"];
+                          value="<? if (isset($arResult["price2"])) {
+                              echo $arResult["price2"];
                           } ?>">
             </td>
         </tr>
         <tr>
             <td>Только в наличии:</td>
             <td><input type="checkbox" name="myFilter[availability]"
-                       <? if (isset($arParams["FILTER"]["availability"])) {
+                       <? if (isset($arResult["availability"])) {
                             echo "checked";
                        } ?>>
             </td>
