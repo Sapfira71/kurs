f9bde9b81b4511e46fb4d1f9305b32136479348a
Morozova Anastasia <a.morozova@maximaster.ru>
2016-03-10 14:27:43 +0300

    Реализовано отображение списка брендов в зависимости от страницы, где находится в данный момент пользователь

--- local/components/maximaster/showbrands/class.php
+++ local/components/maximaster/showbrands/class.php
@@ -7,9 +7,43 @@ use Bitrix\Main\Entity;
 
 class CShowBrands extends CBitrixComponent
 {
+    private function getListCurrentBrands()
+    {
+        $result = Array();
+
+        CModule::IncludeModule('iblock');
+
+        $arFilter = Array(
+            "IBLOCK_ID" => IBLOCK_CATALOG_ID
+        );
+        if (!empty($_REQUEST['SECTION_ID'])) {
+            $arFilter = Array(
+                'SECTION_ID' => $_REQUEST['SECTION_ID'],
+                'INCLUDE_SUBSECTIONS' => 'Y'
+            );
+        }
+        if (!empty($_REQUEST['ELEMENT_ID'])) {
+            $arFilter['ID'] = $_REQUEST['ELEMENT_ID'];
+        }
+        if (!empty($_REQUEST['BRAND_ID'])) {
+            $arFilter['PROPERTY_BRAND'] = $_REQUEST['BRAND_ID'];
+        }
+        $arSelect = Array(
+            'PROPERTY_BRAND'
+        );
+
+        $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+        while ($ob = $res->Fetch()) {
+            $result[] = $ob['PROPERTY_BRAND_VALUE'];
+        }
+
+        return $result;
+    }
+
     public function getBrands()
     {
         $res = Array();
+        $listCurrentBr = $this->getListCurrentBrands();
 
         CModule::IncludeModule("highloadblock");
 
@@ -24,10 +58,15 @@ class CShowBrands extends CBitrixComponent
         ));
         $rsData = new CDBResult($rsData, $sTableID);
         while ($arRes = $rsData->Fetch()) {
-            $res[] = Array(
-                'NAME' => $arRes['UF_NAME'],
-                'XML_ID' => $arRes['UF_XML_ID']
-            );
+            foreach ($listCurrentBr as $br) {
+                if ($arRes['UF_XML_ID'] == $br) {
+                    $res[] = Array(
+                        'NAME' => $arRes['UF_NAME'],
+                        'XML_ID' => $arRes['UF_XML_ID']
+                    );
+                    break;
+                }
+            }
         }
 
         return $res;
