62e39c66a9bc5c7be7c393f7be5f303bcb2619a1
Морозова Анастасия <a.morozova@maximaster.ru>
2016-03-31 13:03:53 +0400

    Изменение компонента показа элемента: добавлен входной параметр.

---
+++ local/components/maximaster/showelement/.parameters.php
@@ -0,0 +1,16 @@
+<?
+$arComponentParameters = array(
+    'GROUPS' => array(
+        'INPUT_PARAMETERS' => array(
+            'NAME' => 'Входные параметры'
+        )
+    ),
+    'PARAMETERS' => array(
+        'ELEMENT_ID' => array(
+            'PARENT' => 'INPUT_PARAMETERS',
+            'NAME' => 'Идентификатор элемента',
+            'TYPE' => 'INT'
+        )
+    )
+);
+?>
