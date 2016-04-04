ed90bff8febd4613b1ac9e5dee87b6f32cc458ab
Морозова Анастасия <a.morozova@maximaster.ru>
2016-04-04 15:24:05 +0400

    Передача имени бренда в качестве параметра компоненту показа секции вместо id

--- local/components/maximaster/complexshowsectionsandelements/class.php
+++ local/components/maximaster/complexshowsectionsandelements/class.php
@@ -35,15 +35,14 @@ class ComplexShowSectionsAndElements extends \CBitrixComponent
     public function executeComponent()
     {
         $request = \Bitrix\Main\Application::getInstance()->getContext()->getRequest();
-        var_dump($request);
         if(isset($request['SECTION_ID'])) {
             $this->arResult['SECTION_ID'] = $request['SECTION_ID'];
         }
         else if(isset($request['CODE'])) {
             $this->arResult['CODE'] = $request['CODE'];
         }
-        else if(isset($request['BRAND_ID'])) {
-            $this->arResult['BRAND_ID'] = $request['BRAND_ID'];
+        else if(isset($request['BRAND_NAME'])) {
+            $this->arResult['BRAND_NAME'] = $request['BRAND_NAME'];
         } else {
             return;
         }
