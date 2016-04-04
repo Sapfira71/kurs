632d9fdf575d2f0cc86e112e3558bbd90b11d76f
Морозова Анастасия <a.morozova@maximaster.ru>
2016-04-04 13:53:29 +0400

    Изменены правила путей до секций и элементов в urlrewrite

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,13 +1,13 @@
 <?
 $arUrlRewrite = array(
     array(
-        'CONDITION' => '#^/catalog/([\-a-zA-Z]+)/#',
-        'RULE' => 'SECTION_ID=$1',
+        'CONDITION' => '#^/catalog/([\/\-a-zA-Z]+)/([0-9]+)/#',
+        'RULE' => 'SECTION_ID=$2',
         'PATH' => '/index.php'
     ),
     array(
-        'CONDITION' => '#^/goods/([\-a-zA-Z]+)/#',
-        'RULE' => 'ELEMENT_ID=$1',
+        'CONDITION' => '#^/goods/([\/\-a-zA-Z]+)/([\-a-zA-Z]+).php#',
+        'RULE' => 'ELEMENT_CODE=$2',
         'PATH' => '/index.php'
     )
 );
