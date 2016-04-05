2deb43dfaf6437a4174e234bf3a5b8675e795e86
Морозова Анастасия <a.morozova@maximaster.ru>
2016-04-05 10:44:35 +0400

    Изменение условия наличия пункта "Главная страница" в верхнем меню из-за изменений в остальных страницах сайта

--- .top.menu.php
+++ .top.menu.php
@@ -5,7 +5,7 @@ $aMenuLinks = Array(
         'index.php',
         Array(),
         Array(),
-        '$_SERVER["REQUEST_URI"]!=="/" ? (strripos($_SERVER["REQUEST_URI"], "index.php") == false ? true : false) : false'
+        '$_SERVER["REQUEST_URI"] !== "/" ? (!strripos($_SERVER["REQUEST_URI"], "index.php") ? ((!isset($_SERVER["REAL_FILE_PATH"]) || !strripos($_SERVER["REAL_FILE_PATH"], "index.php")) ? true : false) : false) : false'
     ),
     Array(
         'О себе',
