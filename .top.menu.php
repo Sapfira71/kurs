2f1ecd97630bfb5641569e6aa2b9cb854388baba
Морозова Анастасия <a.morozova@maximaster.ru>
2016-04-06 16:24:01 +0400

    Доработано условие для "Главная страница".

--- .top.menu.php
+++ .top.menu.php
@@ -5,7 +5,7 @@ $aMenuLinks = Array(
         'index.php',
         Array(),
         Array(),
-        '!strripos($APPLICATION->GetCurUri("", true), "index.php")'
+        '!strripos($APPLICATION->GetCurUri("", true), "index.php") ? (!strripos($_SERVER["REAL_FILE_PATH"], "index.php") ? "1" : "0") : "0"'
     ),
     Array(
         'О себе',
